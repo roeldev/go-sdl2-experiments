d27bde9d46999f3708b5380cf30252d88ce8a5f4
roeldev <roeldev@users.noreply.github.com>
2021-01-21 21:04:05 +0100

    Add getting center point of Viewport

--- view.go
+++ view.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2020, Roel Schut. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package sdlkit
 
 import (
@@ -8,6 +12,10 @@ type Viewport sdl.Rect
 
 func (vp Viewport) Rect() sdl.Rect {
 	return sdl.Rect{X: vp.X, Y: vp.Y, W: vp.W, H: vp.H}
+}
+
+func (vp Viewport) Center() sdl.Point {
+	return sdl.Point{X: vp.X + vp.W/2, Y: vp.Y + vp.H/2}
 }
 
 func (vp Viewport) Border(margin int32) (t, b, l, r int32) {
